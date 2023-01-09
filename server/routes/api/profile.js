0cacf1fd18c51eb412ac6db1e8759de7c4f72125
Hryhorii <hryhorii@Air-Hryhorii.lan>
2023-01-09 17:22:18 +0200

    get github repos for profile

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
@@ -316,5 +318,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     return res.status(500).send('Server Error');
   }
 });
+
+// @route     GET api/profile/github/:username
+// @desc      Get user repos from github
+// @access    Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: {
+        'user-agent': 'node.js'
+      }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(400).json({ msg: 'No github profile found' });
+      }
+
+      return res.json(JSON.parse(body));
+    })
+  } catch (e) {
+    console.error(e.message);
+    return res.status(500).send('Server error');
+  }
+})
 
 module.exports = router;
